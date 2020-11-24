4f380921f5d43dcb64d03be708aa39c6c5840cdb
Jakob Braun <jakob.braun@posteo.de>
2020-11-24 13:00:23 +0100

    small fixes

--- src/test/java/com/exasol/projectkeeper/validators/pom/plugin/FailsafePluginPomValidatorTest.java
+++ src/test/java/com/exasol/projectkeeper/validators/pom/plugin/FailsafePluginPomValidatorTest.java
@@ -1,18 +1,26 @@
 package com.exasol.projectkeeper.validators.pom.plugin;
 
-import static com.exasol.projectkeeper.validators.pom.PomTesting.invalidatePom;
+import static com.exasol.projectkeeper.validators.pom.PomTesting.*;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.not;
+import static org.xmlunit.matchers.HasXPathMatcher.hasXPath;
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.List;
 
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
+import org.xmlunit.matchers.EvaluateXPathMatcher;
+
+import com.exasol.projectkeeper.ProjectKeeperModule;
 
 class FailsafePluginPomValidatorTest extends AbstractMavenPluginPomValidatorTest {
 
@@ -35,5 +43,21 @@ class FailsafePluginPomValidatorTest extends AbstractMavenPluginPomValidatorTest
         final Node plugin = invalidatePom(removeXpath, getFixedPom());
         runFixesFromValidatePluginConfiguration(plugin, Collections.emptyList());
         assertThat(isPluginConfigurationValid(plugin, Collections.emptyList()), equalTo(true));
+    }
+
+    @Test
+    void testFixWithoutUdfIntegration() throws ParserConfigurationException, SAXException, IOException {
+        final Document pom = readXmlFromResources(POM_WITH_NO_PLUGINS);
+        runAllFixesGeneratedByValidation(pom, Collections.emptyList());
+        assertThat(pom,
+                not(hasXPath("/project/build/plugins/plugin/configuration/systemPropertyVariables/test.coverage")));
+    }
+
+    @Test
+    void testFixWithUdfIntegration() throws ParserConfigurationException, SAXException, IOException {
+        final Document pom = readXmlFromResources(POM_WITH_NO_PLUGINS);
+        runAllFixesGeneratedByValidation(pom, List.of(ProjectKeeperModule.UDF_COVERAGE));
+        assertThat(pom, EvaluateXPathMatcher.hasXPath(
+                "/project/build/plugins/plugin/configuration/systemPropertyVariables/test.coverage", equalTo("true")));
     }
 }
