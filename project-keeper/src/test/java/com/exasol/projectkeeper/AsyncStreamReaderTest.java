e1953784046ddb24636d6fe2ae0fe2578b45bd80
Christoph Pirkl <christoph.pirkl@exasol.com>
2022-02-07 10:44:56 +0100

    Fix sonar finding

--- project-keeper/src/test/java/com/exasol/projectkeeper/AsyncStreamReaderTest.java
+++ project-keeper/src/test/java/com/exasol/projectkeeper/AsyncStreamReaderTest.java
@@ -10,7 +10,7 @@ import com.exasol.projectkeeper.AsyncStreamReader.CollectingConsumer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class AsyncStreamReaderTest {
+class AsyncStreamReaderTest {
 
     private AsyncStreamReader asyncStreamReader;
 
