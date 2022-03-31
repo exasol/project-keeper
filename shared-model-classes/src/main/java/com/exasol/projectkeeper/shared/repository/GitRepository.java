7115a7577a63b653b452935cd9ff83204fdae0c6
Christoph Pirkl <christoph.pirkl@exasol.com>
2022-03-31 15:22:09 +0200

    Refactor GitRepository

--- shared-model-classes/src/main/java/com/exasol/projectkeeper/shared/repository/GitRepository.java
+++ shared-model-classes/src/main/java/com/exasol/projectkeeper/shared/repository/GitRepository.java
@@ -1,6 +1,7 @@
 package com.exasol.projectkeeper.shared.repository;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
@@ -12,33 +13,59 @@ import org.eclipse.jgit.transport.RemoteConfig;
 import org.eclipse.jgit.treewalk.TreeWalk;
 
 import com.exasol.errorreporting.ExaError;
+import com.exasol.projectkeeper.shared.ExasolVersionMatcher;
 
 /**
  * This gives access to a local git repository.
  */
-public class GitRepository {
+public class GitRepository implements AutoCloseable {
     private final Path projectDirectory;
+    private final Git git;
+
+    private GitRepository(final Git git, final Path projectDirectory) {
+        this.git = git;
+        this.projectDirectory = projectDirectory;
+    }
 
     /**
-     * Create a new instance of {@link GitRepository}.
-     * 
+     * Open the given git repository and create a new instance of {@link GitRepository}.
+     *
      * @param projectDirectory the projects directory.
      */
-    public GitRepository(final Path projectDirectory) {
-        this.projectDirectory = projectDirectory;
+    public static GitRepository open(final Path projectDirectory) {
+        return new GitRepository(openLocalGitRepository(projectDirectory), projectDirectory);
+    }
+
+    private static Git openLocalGitRepository(final Path projectDirectory) {
+        try {
+            return Git.open(projectDirectory.toFile());
+        } catch (final IOException exception) {
+            throw new IllegalStateException(ExaError.messageBuilder("E-PK-SMC-32")
+                    .message("Failed to open local git repository {{repository}}.")
+                    .mitigation("If this is a new project you maybe need to create the git project using `git init`.")
+                    .parameter("repository", projectDirectory.toString()).toString(), exception);
+        }
+    }
+
+    public Optional<TaggedCommit> findLatestReleaseCommit(final String currentVersion) {
+        final var exasolVersionMatcher = new ExasolVersionMatcher();
+        return this.getTagsInCurrentBranch().stream()
+                .filter(taggedCommit -> exasolVersionMatcher.isExasolStyleVersion(taggedCommit.getTag()))//
+                .filter(taggedCommit -> !taggedCommit.getTag().equals(currentVersion))//
+                .findFirst();
     }
 
     /**
      * Get the tags of the current branch ordered descending by commit date. If one commit has multiple tags, they are
      * returned sequential with no specified order.
-     * 
+     *
      * @return descending ordered list of tags
      */
     public List<TaggedCommit> getTagsInCurrentBranch() {
-        try (final var git = openLocalGithubRepository()) {
-            final String currentBranch = git.getRepository().getFullBranch();
+        try {
+            final String currentBranch = this.git.getRepository().getFullBranch();
             validateBranchExists(currentBranch);
-            return getTagsInBranch(git.getRepository(), currentBranch);
+            return getTagsInBranch(this.git.getRepository(), currentBranch);
         } catch (final IOException exception) {
             throw new IllegalStateException(
                     ExaError.messageBuilder("E-PK-SMC-33")
@@ -59,15 +86,16 @@ public class GitRepository {
             throws IOException {
         final ObjectId branch = repository.resolve(branchName);
         final Map<ObjectId, List<String>> tags = getTagsByTheIdOfTheCommitTheyPointTo(repository);
-        final var commitWalker = new RevWalk(repository);
-        try {
-            commitWalker.markStart(commitWalker.parseCommit(branch));
-        } catch (final NullPointerException exception) {
-            // NullPointerException is thrown if the branch has no commits
-            return Collections.emptyList();
+        try (final var commitWalker = new RevWalk(repository)) {
+            try {
+                commitWalker.markStart(commitWalker.parseCommit(branch));
+            } catch (final NullPointerException exception) {
+                // NullPointerException is thrown if the branch has no commits
+                return Collections.emptyList();
+            }
+            commitWalker.sort(RevSort.COMMIT_TIME_DESC);
+            return readTagsFromCommits(tags, commitWalker);
         }
-        commitWalker.sort(RevSort.COMMIT_TIME_DESC);
-        return readTagsFromCommits(tags, commitWalker);
     }
 
     private List<TaggedCommit> readTagsFromCommits(final Map<ObjectId, List<String>> tags, final RevWalk commitWalker) {
@@ -110,17 +138,42 @@ public class GitRepository {
     }
 
     /**
-     * Read a specific file at a given commit.
-     * 
+     * Read a specific file at a given commit and extract it to a target file.
+     *
      * @param relativeFilePath file path
      * @param commit           commit
-     * @param targetFile       path where this method writes the contet to
+     * @param targetFile       path where this method writes the content to
      * @throws FileNotFoundException if the file does not exist in the repo
      */
     public void extractFileFromCommit(final Path relativeFilePath, final GitCommit commit, final Path targetFile)
             throws FileNotFoundException {
-        try (final var git = openLocalGithubRepository()) {
-            extractFileFromCommit(relativeFilePath, git, commit.getCommit(), targetFile);
+        try {
+            extractFileFromCommit(relativeFilePath, this.git, commit.getCommit(), targetFile);
+        } catch (final FileNotFoundException exception) {
+            throw exception;
+        } catch (final IOException exception) {
+            throw new IllegalStateException(
+                    ExaError.messageBuilder("E-PK-SMC-43")
+                            .message("Failed to retrieve latest tag from the local git repository.").toString(),
+                    exception);
+        }
+    }
+
+    /**
+     * Read a specific file at a given commit and return its content.
+     *
+     * @param relativeFilePath file path
+     * @param commit           commit
+     * @throws FileNotFoundException if the file does not exist in the repo
+     */
+    public String getFileFromCommit(final Path relativeFilePath, final GitCommit commit) throws FileNotFoundException {
+        try {
+            final var repository = this.git.getRepository();
+            final ObjectId objectForVersionOfFile = findFile(commit.getCommit(), repository, relativeFilePath);
+            try (final var reader = repository.newObjectReader()) {
+                final var objectLoader = reader.open(objectForVersionOfFile);
+                return new String(objectLoader.getBytes(), StandardCharsets.UTF_8);
+            }
         } catch (final FileNotFoundException exception) {
             throw exception;
         } catch (final IOException exception) {
@@ -155,18 +208,19 @@ public class GitRepository {
     private ObjectId findFile(final RevCommit commit, final Repository repository, final Path expectedPath)
             throws IOException {
         final Path normalizedExpected = expectedPath.normalize();
-        final TreeWalk treeWalk = new TreeWalk(repository);
-        treeWalk.addTree(commit.getTree());
-        treeWalk.setRecursive(true);
-        while (treeWalk.next()) {
-            final Path currentPath = Path.of(treeWalk.getPathString()).normalize();
-            if (treeWalk.isSubtree()) {
-                if (normalizedExpected.startsWith(currentPath)) {
-                    treeWalk.enterSubtree();
-                }
-            } else {
-                if (normalizedExpected.equals(currentPath)) {
-                    return treeWalk.getObjectId(0);
+        try (final TreeWalk treeWalk = new TreeWalk(repository)) {
+            treeWalk.addTree(commit.getTree());
+            treeWalk.setRecursive(true);
+            while (treeWalk.next()) {
+                final Path currentPath = Path.of(treeWalk.getPathString()).normalize();
+                if (treeWalk.isSubtree()) {
+                    if (normalizedExpected.startsWith(currentPath)) {
+                        treeWalk.enterSubtree();
+                    }
+                } else {
+                    if (normalizedExpected.equals(currentPath)) {
+                        return treeWalk.getObjectId(0);
+                    }
                 }
             }
         }
@@ -176,25 +230,14 @@ public class GitRepository {
                 .mitigation("Make sure that the file exists at the given commit.").toString());
     }
 
-    private Git openLocalGithubRepository() {
-        try {
-            return Git.open(this.projectDirectory.toFile());
-        } catch (final IOException exception) {
-            throw new IllegalStateException(ExaError.messageBuilder("E-PK-SMC-32")
-                    .message("Failed to open local git repository {{repository}}.")
-                    .mitigation("If this is a new project you maybe need to create the git project using `git init`.")
-                    .parameter("repository", this.projectDirectory.toString()).toString(), exception);
-        }
-    }
-
     /**
      * Try to get a the name of the repository from the git remote configuration.
-     * 
+     *
      * @return repository name
      */
     public Optional<String> getRepoNameFromRemote() {
         try {
-            final List<RemoteConfig> remotes = openLocalGithubRepository().remoteList().call();
+            final List<RemoteConfig> remotes = this.git.remoteList().call();
             final Optional<RemoteConfig> origin = remotes.stream().filter(remote -> remote.getName().equals("origin"))
                     .findAny();
             if (origin.isPresent()) {
@@ -208,5 +251,10 @@ public class GitRepository {
         } catch (final Exception exception) {
             return Optional.empty();
         }
+    }
+
+    @Override
+    public void close() {
+        this.git.close();
     }
 }
