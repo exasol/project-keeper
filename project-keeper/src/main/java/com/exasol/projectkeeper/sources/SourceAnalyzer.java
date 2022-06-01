6398660dfba9ed965ff0fec36a37a9b360391410
Christoph Pirkl <christoph.pirkl@exasol.com>
2022-06-01 15:44:55 +0200

    #319: Analyze sources in correct order

--- project-keeper/src/main/java/com/exasol/projectkeeper/sources/SourceAnalyzer.java
+++ project-keeper/src/main/java/com/exasol/projectkeeper/sources/SourceAnalyzer.java
@@ -1,10 +1,13 @@
 package com.exasol.projectkeeper.sources;
 
 import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
 
 import java.nio.file.Path;
 import java.util.*;
 import java.util.Map.Entry;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import com.exasol.errorreporting.ExaError;
 import com.exasol.projectkeeper.shared.config.ProjectKeeperConfig;
@@ -20,14 +23,14 @@ import com.exasol.projectkeeper.sources.analyze.golang.GolangSourceAnalyzer;
 public class SourceAnalyzer {
     private final Map<SourceType, LanguageSpecificSourceAnalyzer> sourceAnalyzers;
 
-    private SourceAnalyzer(final Map<SourceType, LanguageSpecificSourceAnalyzer> sourceAnalyzers) {
+    SourceAnalyzer(final Map<SourceType, LanguageSpecificSourceAnalyzer> sourceAnalyzers) {
         this.sourceAnalyzers = sourceAnalyzers;
     }
 
     /**
      * Creates a new instance of {@link SourceAnalyzer}, configuring the available
      * {@link LanguageSpecificSourceAnalyzer}s.
-     * 
+     *
      * @param config     project keeper configuration passed to the {@link LanguageSpecificSourceAnalyzer}s if necessary
      * @param mvnRepo    the path to the maven repository, may be {@code null} if the default should be used
      * @param ownVersion the version of this running project keeper instance
@@ -49,18 +52,54 @@ public class SourceAnalyzer {
      *
      * @param projectDir project directory
      * @param sources    configured sources
-     * @return analyzed sources
+     * @return analyzed sources in the same order in the input
      */
     public List<AnalyzedSource> analyze(final Path projectDir, final List<ProjectKeeperConfig.Source> sources) {
+        validateUniqueSourcePaths(sources);
+        final Map<Path, AnalyzedSource> analyzedSources = analyzeSources(projectDir, sources);
+        return fixOrder(sources, analyzedSources);
+    }
+
+    private Map<Path, AnalyzedSource> analyzeSources(final Path projectDir,
+            final List<ProjectKeeperConfig.Source> sources) {
         final Map<SourceType, List<Source>> groupedSources = sources.stream()
                 .collect(groupingBy(ProjectKeeperConfig.Source::getType));
-
-        final List<AnalyzedSource> analyzedSources = new ArrayList<>(sources.size());
+        final Map<Path, AnalyzedSource> analyzedSources = new HashMap<>(sources.size());
         for (final Entry<SourceType, List<Source>> entry : groupedSources.entrySet()) {
             final LanguageSpecificSourceAnalyzer analyzer = getAnalyzer(entry.getKey());
-            analyzedSources.addAll(analyzer.analyze(projectDir, entry.getValue()));
+            analyzer.analyze(projectDir, entry.getValue())
+                    .forEach(source -> analyzedSources.put(source.getPath(), source));
         }
         return analyzedSources;
+    }
+
+    private List<AnalyzedSource> fixOrder(final List<ProjectKeeperConfig.Source> sources,
+            final Map<Path, AnalyzedSource> analyzedSources) {
+        return sources.stream().map(source -> findSource(source, analyzedSources)).collect(toList());
+    }
+
+    private AnalyzedSource findSource(final Source source, final Map<Path, AnalyzedSource> analyzedSources) {
+        final AnalyzedSource analyzedSource = analyzedSources.get(source.getPath());
+        if (analyzedSource != null) {
+            return analyzedSource;
+        } else {
+            throw new IllegalStateException(ExaError.messageBuilder("E-PK-CORE-150").message(
+                    "Did not find analyzed source for path {{path}}, all analyzed sources: {{analyzed sources}}",
+                    source.getPath(), analyzedSources).ticketMitigation().toString());
+        }
+    }
+
+    private void validateUniqueSourcePaths(final List<Source> sources) {
+        final Map<Path, Long> sourceFrequencies = sources.stream().map(Source::getPath)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        final List<Path> duplicatePaths = sourceFrequencies.entrySet().stream() //
+                .filter(entry -> entry.getValue() > 1) //
+                .map(Entry::getKey).collect(toList());
+        if (!duplicatePaths.isEmpty()) {
+            throw new IllegalStateException(ExaError.messageBuilder("E-PK-CORE-149")
+                    .message("Sources use duplicate paths {{duplicate paths}}.", duplicatePaths)
+                    .mitigation("Make sure that sources in .project-keeper.yml have unique paths.").toString());
+        }
     }
 
     private LanguageSpecificSourceAnalyzer getAnalyzer(final SourceType type) {
---
+++ project-keeper/src/test/java/com/exasol/projectkeeper/sources/SourceAnalyzerTest.java
@@ -0,0 +1,113 @@
+package com.exasol.projectkeeper.sources;
+
+import static java.util.Collections.emptyList;
+import static java.util.stream.Collectors.toList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.startsWith;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.exasol.projectkeeper.shared.config.ProjectKeeperConfig.Source;
+import com.exasol.projectkeeper.shared.config.ProjectKeeperConfig.SourceType;
+import com.exasol.projectkeeper.sources.analyze.LanguageSpecificSourceAnalyzer;
+
+@ExtendWith(MockitoExtension.class)
+class SourceAnalyzerTest {
+
+    private static final Path PROJECT_DIR = null;
+    @Mock
+    private LanguageSpecificSourceAnalyzer golangAnalyzerMock;
+    @Mock
+    private LanguageSpecificSourceAnalyzer mavenAnalyzerMock;
+
+    @BeforeEach
+    void setup() {
+        lenient().when(this.golangAnalyzerMock.analyze(eq(PROJECT_DIR), anyList())).thenAnswer(this::analyzerAnswer);
+        lenient().when(this.mavenAnalyzerMock.analyze(eq(PROJECT_DIR), anyList())).thenAnswer(this::analyzerAnswer);
+    }
+
+    private List<AnalyzedSource> analyzerAnswer(final InvocationOnMock invocation) {
+        @SuppressWarnings("unchecked")
+        final List<Source> sources = invocation.getArgument(1, List.class);
+        return sources.stream().map(Source.class::cast) //
+                .map(this::analyzedSource) //
+                .collect(toList());
+    }
+
+    @Test
+    void emptySourceList() {
+        final SourceAnalyzer analyzer = createWithAllAnalyzers();
+        assertThat(analyzer.analyze(PROJECT_DIR, emptyList()), hasSize(0));
+    }
+
+    @Test
+    void duplicatePathsThrowsException() {
+        final SourceAnalyzer analyzer = createWithAllAnalyzers();
+        final List<Source> sources = List.of(source("path", SourceType.MAVEN), source("path", SourceType.GOLANG));
+        final IllegalStateException exception = assertThrows(IllegalStateException.class,
+                () -> analyzer.analyze(PROJECT_DIR, sources));
+        assertThat(exception.getMessage(), equalTo(
+                "E-PK-CORE-149: Sources use duplicate paths [path]. Make sure that sources in .project-keeper.yml have unique paths."));
+    }
+
+    @Test
+    void sourceNotfound() {
+        final SourceAnalyzer analyzer = createWithAllAnalyzers();
+        final List<Source> sources = List.of(source("path", SourceType.MAVEN));
+        when(this.mavenAnalyzerMock.analyze(any(), any())).thenReturn(List.of(analyzedSource(Paths.get("wrong path"))));
+        final IllegalStateException exception = assertThrows(IllegalStateException.class,
+                () -> analyzer.analyze(PROJECT_DIR, sources));
+        assertThat(exception.getMessage(),
+                startsWith("E-PK-CORE-150: Did not find analyzed source for path path, all analyzed sources:"));
+    }
+
+    @RepeatedTest(10)
+    void analyzeOrderIsStable() {
+        final SourceAnalyzer analyzer = createWithAllAnalyzers();
+        final List<AnalyzedSource> analyzedSources = analyzer.analyze(PROJECT_DIR,
+                List.of(source("maven1", SourceType.MAVEN), //
+                        source("go1", SourceType.GOLANG), //
+                        source("maven2", SourceType.MAVEN), //
+                        source("go2", SourceType.GOLANG)));
+        assertAll(() -> assertThat(analyzedSources, hasSize(4)),
+                () -> assertThat(analyzedSources.get(0).getPath().toString(), equalTo("maven1")),
+                () -> assertThat(analyzedSources.get(1).getPath().toString(), equalTo("go1")),
+                () -> assertThat(analyzedSources.get(2).getPath().toString(), equalTo("maven2")),
+                () -> assertThat(analyzedSources.get(3).getPath().toString(), equalTo("go2")));
+    }
+
+    private AnalyzedSource analyzedSource(final Source originalSource) {
+        return analyzedSource(originalSource.getPath());
+    }
+
+    private AnalyzedSource analyzedSource(final Path path) {
+        return new AnalyzedMavenSource(path, null, false, null, null, null, null, null, false);
+    }
+
+    private Source source(final String path, final SourceType type) {
+        return Source.builder().path(Paths.get(path)).type(type).build();
+    }
+
+    private SourceAnalyzer createWithAllAnalyzers() {
+        return new SourceAnalyzer(
+                Map.of(SourceType.GOLANG, this.golangAnalyzerMock, SourceType.MAVEN, this.mavenAnalyzerMock));
+    }
+}
