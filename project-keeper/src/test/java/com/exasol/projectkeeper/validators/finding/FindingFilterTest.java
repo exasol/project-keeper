1bb4db0631145d5c4f123863a4de3adda5497388
KK <christoph.kuhnke@exasol.com>
2022-08-24 08:38:50 +0200

    test

--- project-keeper/src/test/java/com/exasol/projectkeeper/validators/finding/FindingFilterTest.java
+++ project-keeper/src/test/java/com/exasol/projectkeeper/validators/finding/FindingFilterTest.java
@@ -12,13 +12,13 @@ class FindingFilterTest {
     private static final SimpleValidationFinding FINDING_1 = SimpleValidationFinding.withMessage("message 1").build();
     private static final SimpleValidationFinding FINDING_2 = SimpleValidationFinding.withMessage("message 2").build();
     private static final SimpleValidationFinding FINDING_3 = SimpleValidationFinding.withMessage("message 3").build();
-    private static final SimpleValidationFinding FINDING_3A = SimpleValidationFinding
+    private static final SimpleValidationFinding FINDING_3_MULTILINE = SimpleValidationFinding
             .withMessage("message 3\nnext line").build();
-    private static final SimpleValidationFinding FINDING_4 = SimpleValidationFinding.withMessage("other").build();
+    private static final SimpleValidationFinding OTHER = SimpleValidationFinding.withMessage("other").build();
     private static final Runnable POST_FIX = () -> {
     };
     private static final ValidationFindingGroup FINDING_GROUP = new ValidationFindingGroup(
-            List.of(FINDING_3, FINDING_3A, FINDING_4), POST_FIX);
+            List.of(FINDING_3, FINDING_3_MULTILINE, OTHER), POST_FIX);
 
     @Test
     void testFilterSimpleFinding() {
@@ -30,15 +30,15 @@ class FindingFilterTest {
     @Test
     void testFilterByRegex() {
         final List<ValidationFinding> result = new FindingFilter(List.of("message .*"))
-                .filterFindings(List.of(FINDING_1, FINDING_2, FINDING_3A, FINDING_4));
-        assertThat(result, contains(FINDING_4));
+                .filterFindings(List.of(FINDING_1, FINDING_2, FINDING_3_MULTILINE, OTHER));
+        assertThat(result, contains(OTHER));
     }
 
     @Test
     void testFilterFindingGroup() {
         final List<ValidationFinding> result = new FindingFilter(List.of("\\Qmessage 3\\E"))
                 .filterFindings(List.of(FINDING_GROUP));
-        assertThat(result, contains(new ValidationFindingGroup(List.of(FINDING_3A, FINDING_4), POST_FIX)));
+        assertThat(result, contains(new ValidationFindingGroup(List.of(FINDING_3_MULTILINE, OTHER), POST_FIX)));
     }
 
     @Test
