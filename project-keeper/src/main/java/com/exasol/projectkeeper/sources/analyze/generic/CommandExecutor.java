8bc16665734e85372220236a7956c5876ab25155
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-02-01 09:15:05 +0100

    Reduce verbosity

--- project-keeper/src/main/java/com/exasol/projectkeeper/sources/analyze/generic/CommandExecutor.java
+++ project-keeper/src/main/java/com/exasol/projectkeeper/sources/analyze/generic/CommandExecutor.java
@@ -1,7 +1,6 @@
 package com.exasol.projectkeeper.sources.analyze.generic;
 
 import java.nio.file.Path;
-import java.util.logging.Logger;
 
 import com.exasol.projectkeeper.sources.analyze.golang.SimpleProcess;
 
@@ -9,7 +8,6 @@ import com.exasol.projectkeeper.sources.analyze.golang.SimpleProcess;
  * Enable to execute a {@link ShellCommand} and hence to mock this in tests.
  */
 public class CommandExecutor {
-    static final Logger LOGGER = Logger.getLogger(CommandExecutor.class.getName());
 
     /**
      * Executes the specified {@link ShellCommand}
@@ -20,7 +18,6 @@ public class CommandExecutor {
      * @throws IllegalStateException if execution fails.
      */
     public String execute(final ShellCommand sc, final Path workingDirectory) throws IllegalStateException {
-        LOGGER.finest(() -> "Executing command " + sc.name());
         final SimpleProcess process = SimpleProcess.start(workingDirectory, sc.commandline());
         process.waitUntilFinished(sc.timeout());
         return process.getOutputStreamContent();
