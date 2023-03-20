417a89fe271b991d166908dcd84931b515dd7296
KK <christoph.kuhnke@exasol.com>
2023-03-20 13:24:57 +0100

    fixed javadoc

--- project-keeper/src/main/java/com/exasol/projectkeeper/validators/dependencies/renderer/Workarounds.java
+++ project-keeper/src/main/java/com/exasol/projectkeeper/validators/dependencies/renderer/Workarounds.java
@@ -1,22 +1,20 @@
 package com.exasol.projectkeeper.validators.dependencies.renderer;
 
+import java.util.Arrays;
+
 /**
- * For a few particular licenses issue #436 describes inconsistent results in file dependencies.md. Sometimes the
- * results will use name "The Apache Software License" while in other cases the name will be "Apache License".
- *
- * <p>
- * Even careful and extensive investigations did not identify the root cause of this problem.
- *
- * <p>
- * The current solution is a workaround simply replacing the one string by the other to have at least consistent and
- * stable results.
- *
- * <p>
- * This class serves as a marker to easily find all places affected by this issue and the workaround.
+ * This class serves as a marker to easily find all places affected by a particular workaround.
  */
 public enum Workarounds {
 
-    ALTERNATIVING_DEPENDENCIES(State.ACTIVE, "");
+    ALTERNATIVING_DEPENDENCIES(State.ACTIVE,
+            description(
+                    "For a few particular licenses issue #436 describes inconsistent results in file dependencies.md.",
+                    "Sometimes the results will use name 'The Apache Software License'",
+                    " while in other cases the name will be 'Apache License'.",
+                    "Even careful and extensive investigations did not identify the root cause of this problem.",
+                    "The current solution is a workaround simply replacing the one string by the",
+                    " other to have at least consistent and stable results."));
 
     private final State state;
     @SuppressWarnings("unused")
@@ -28,7 +26,7 @@ public enum Workarounds {
     }
 
     /**
-     * @return {@link true} if the current workaround is active
+     * @return {@code true} if the current workaround is active
      */
     public boolean isActive() {
         return this.state == State.ACTIVE;
@@ -36,5 +34,9 @@ public enum Workarounds {
 
     enum State {
         ACTIVE, DISABLED;
+    }
+
+    private static String description(final String... line) {
+        return String.join("\n", Arrays.asList(line));
     }
 }
