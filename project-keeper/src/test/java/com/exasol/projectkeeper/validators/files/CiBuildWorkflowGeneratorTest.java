34bcfd9ef6196f53724cd2314b2d1ab678a5294c
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-11-13 09:09:11 +0100

    Adapt tests for windows

--- project-keeper/src/test/java/com/exasol/projectkeeper/validators/files/CiBuildWorkflowGeneratorTest.java
+++ project-keeper/src/test/java/com/exasol/projectkeeper/validators/files/CiBuildWorkflowGeneratorTest.java
@@ -13,6 +13,8 @@ import com.exasol.projectkeeper.shared.config.BuildConfig;
 
 class CiBuildWorkflowGeneratorTest {
 
+    private static String NL = System.lineSeparator();
+
     @Test
     void rdOrgChecksumContainsRunnerOs() {
         assertThat(releaseDroidOriginalChecksumContent(BuildConfig.builder().runnerOs("my-runner-os")),
@@ -22,14 +24,14 @@ class CiBuildWorkflowGeneratorTest {
     @Test
     void rdOrgChecksumDoesNotFreeDiskSpace() {
         assertThat(releaseDroidOriginalChecksumContent(BuildConfig.builder().freeDiskSpace(false)),
-                containsString("- name: Free Disk Space\n" + //
+                containsString("- name: Free Disk Space" + NL + //
                         "        if: ${{ false }}"));
     }
 
     @Test
     void rdOrgChecksumDoesFreeDiskSpace() {
         assertThat(releaseDroidOriginalChecksumContent(BuildConfig.builder().freeDiskSpace(true)),
-                containsString("- name: Free Disk Space\n" + //
+                containsString("- name: Free Disk Space" + NL + //
                         "        if: ${{ true }}"));
     }
 
@@ -42,21 +44,21 @@ class CiBuildWorkflowGeneratorTest {
     @Test
     void ciBuildDoesNotFreeDiskSpace() {
         assertThat(ciBuildContent(BuildConfig.builder().freeDiskSpace(false)),
-                containsString("- name: Free Disk Space\n" + //
+                containsString("- name: Free Disk Space" + NL + //
                         "        if: ${{ false }}"));
     }
 
     @Test
     void ciBuildDoesFreeDiskSpace() {
         assertThat(ciBuildContent(BuildConfig.builder().freeDiskSpace(true)),
-                containsString("- name: Free Disk Space\n" + //
+                containsString("- name: Free Disk Space" + NL + //
                         "        if: ${{ true }}"));
     }
 
     @Test
     void ciBuildNonMatrixBuild() {
         assertThat(ciBuildContent(BuildConfig.builder()), allOf( //
-                containsString("group: ${{ github.workflow }}-${{ github.ref }}\n"), //
+                containsString("group: ${{ github.workflow }}-${{ github.ref }}" + NL), //
                 not(containsString("matrix")), //
                 not(containsString("com.exasol.dockerdb.image")) //
         ));
@@ -65,12 +67,12 @@ class CiBuildWorkflowGeneratorTest {
     @Test
     void ciBuildMatrixBuild() {
         assertThat(ciBuildContent(BuildConfig.builder().exasolDbVersions(List.of("v1", "v2"))), allOf( //
-                containsString("group: ${{ github.workflow }}-${{ github.ref }}-${{ matrix.exasol_db_version }}\n"), //
-                containsString("matrix:\n        exasol_db_version: [\"v1\", \"v2\"]"), //
-                containsString("env:\n      DEFAULT_EXASOL_DB_VERSION: \"v1\""),
+                containsString("group: ${{ github.workflow }}-${{ github.ref }}-${{ matrix.exasol_db_version }}" + NL), //
+                containsString("matrix:" + NL + "        exasol_db_version: [\"v1\", \"v2\"]"), //
+                containsString("env:" + NL + "      DEFAULT_EXASOL_DB_VERSION: \"v1\""),
                 containsString("-Dcom.exasol.dockerdb.image=${{ matrix.exasol_db_version }}"), //
-                containsString(
-                        "- name: Sonar analysis\n        if: ${{ env.SONAR_TOKEN != null && matrix.exasol_db_version == env.DEFAULT_EXASOL_DB_VERSION }}") //
+                containsString("- name: Sonar analysis" + NL
+                        + "        if: ${{ env.SONAR_TOKEN != null && matrix.exasol_db_version == env.DEFAULT_EXASOL_DB_VERSION }}") //
         ));
     }
 
