faa42a30f5905a46240d745784f752408d4ecb94
Christoph Pirkl <4711730+kaklakariada@users.noreply.github.com>
2026-04-29 13:16:02 +0200

    #725: Fixed parsing NPM license arrays (#726)

--- project-keeper/src/main/java/com/exasol/projectkeeper/sources/analyze/npm/NpmLicense.java
+++ project-keeper/src/main/java/com/exasol/projectkeeper/sources/analyze/npm/NpmLicense.java
@@ -1,27 +1,62 @@
 package com.exasol.projectkeeper.sources.analyze.npm;
 
+import static java.util.stream.Collectors.toMap;
+
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import com.exasol.projectkeeper.shared.dependencies.License;
 
-import jakarta.json.JsonObject;
+import jakarta.json.*;
 
 //[impl -> dsn~npm-dependency-licenses~1]
 record NpmLicense(String module, String version, String name, String url) {
 
     private static final Pattern PATTERN = Pattern.compile("^(.+)@([0-9]+(\\.[0-9]+)*+)$");
 
-    static NpmLicense from(final String moduleAndVersion, final JsonObject licenseInfos) {
+    static Map<String, List<NpmLicense>> from(final JsonObject json) {
+        return json.keySet().stream()
+                .collect(toMap(NpmLicense::moduleName, key -> extract(key, json), NpmLicense::merge));
+    }
+
+    private static List<NpmLicense> extract(final String moduleAndVersion, final JsonObject licenseInfos) {
         final JsonObject o = licenseInfos.getJsonObject(moduleAndVersion);
-        final String name = jString(o, "licenses");
         final String mainUrl = jString(o, "repository");
         final String[] p = split(moduleAndVersion);
-        return new NpmLicense(p[0], p[1], name, mainUrl);
+        final String module = p[0];
+        final String version = p[1];
+        return jStringList(o, "licenses").stream()
+                .map(name -> new NpmLicense(module, version, name, mainUrl))
+                .toList();
+    }
+
+    private static String moduleName(final String moduleAndVersion) {
+        return split(moduleAndVersion)[0];
+    }
+
+    private static List<NpmLicense> merge(final List<NpmLicense> left, final List<NpmLicense> right) {
+        final List<NpmLicense> merged = new ArrayList<>(left);
+        merged.addAll(right);
+        return merged;
     }
 
     private static String jString(final JsonObject o, final String key) {
         return o.containsKey(key) ? o.getString(key) : null;
+    }
+
+    private static List<String> jStringList(final JsonObject o, final String key) {
+        if (!o.containsKey(key)) {
+            return Collections.singletonList(null);
+        }
+        final JsonValue value = o.get(key);
+        if (value.getValueType() == JsonValue.ValueType.ARRAY) {
+            return value.asJsonArray().getValuesAs(JsonString.class).stream()
+                    .map(JsonString::getString)
+                    .toList();
+        } else {
+            return List.of(((JsonString) value).getString());
+        }
     }
 
     private static String[] split(final String moduleAndVersion) {
@@ -29,10 +64,6 @@ record NpmLicense(String module, String version, String name, String url) {
         return matcher.matches() //
                 ? new String[] { matcher.group(1), matcher.group(2) }
                 : new String[] { moduleAndVersion, null };
-    }
-
-    String getModule() {
-        return this.module;
     }
 
     String getVersion() {
